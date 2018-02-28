1bbd92eb372076a18454f361228531be89946087
Soenke Ruempler <soenke.ruempler@superluminar.io>
2018-02-28 09:14:02 +0100

    make it possible to use the tool with client secret

--- get_id_token/get_id_token.py
+++ get_id_token/get_id_token.py
@@ -10,11 +10,12 @@ argp.add_argument('-u', '--username', required=True, help='Cognito Userpool user
 argp.add_argument('-p', '--password', required=True, help='Cognito Userpool password')
 argp.add_argument('-i', '--user-pool-id', required=True, help='Cognito Userpool ID')
 argp.add_argument('-c', '--client-id', required=True, help='Cognito Userpool Client ID')
+argp.add_argument('-s', '--client-secret', required=False, help='Cognito Userpool Client secret')
 args=argp.parse_args()
 
 boto_client = boto3.client('cognito-idp', config=Config(region_name=args.user_pool_id.split('_')[0], signature_version=UNSIGNED))
 aws = AWSSRP(username=args.username, password=args.password, pool_id=args.user_pool_id,
-             client_id=args.client_id, client=boto_client)
+             client_id=args.client_id, client_secret=args.client_secret, client=boto_client)
 tokens = aws.authenticate_user()
 
 id_token = tokens['AuthenticationResult']['IdToken']
